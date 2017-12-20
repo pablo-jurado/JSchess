11a836456c7de8f39507d898adc5f7d97049391b
Pablo Jurado <pjurado85@gmail.com>
2017-12-19 21:25:58 -0600

    add rook rules

--- js/chess.js
+++ js/chess.js
@@ -1,6 +1,6 @@
-// -----------------------------------------------------------------------------
+// ---------------------------------------------------
 // Constants
-// -----------------------------------------------------------------------------
+// ---------------------------------------------------
 const BOARD_DOM = document.getElementById('app');
 
 const ROW = 8;
@@ -11,7 +11,7 @@ const BOARD_BORDER = 10;
 const BLACK = 'b';
 const WHITE = 'w';
 
-// pieces
+// names
 const PAWN = 'pawn';
 const ROOK = 'rook';
 const KNIGHT = 'knight';
@@ -19,15 +19,29 @@ const BISHOP = 'bishop';
 const QUEEN = 'queen';
 const KING = 'king';
 
-// -----------------------------------------------------------------------------
+// pieces
+const B_PAWN = '&#9823;';
+const W_PAWN = '&#9817;';
+const B_ROOK = '&#9820;';
+const W_ROOK = '&#9814;';
+const B_KNIGHT = '&#9822;';
+const W_KNIGHT = '&#9816;';
+const B_BISHOP = '&#9821;';
+const W_BISHOP = '&#9815;';
+const B_QUEEN = '&#9819;';
+const W_QUEEN = '&#9813;';
+const B_KING = '&#9818;';
+const W_KING = '&#9812;';
+
+// ---------------------------------------------------
 // Variables
-// -----------------------------------------------------------------------------
+// ---------------------------------------------------
 var isMoving = null;
 var turnDom = document.getElementById('turn');
 
-// -----------------------------------------------------------------------------
+// ---------------------------------------------------
 
-function createBoard (row, col) {
+function createBoard(row, col) {
   var arr = [];
   for (var i = 0; i < row; i++) {
     var rowArr = [];
@@ -42,9 +56,9 @@ function createBoard (row, col) {
   return arr;
 }
 
-function renderBoard () {
-  var boardHTML = this.board.reduce(function (acc, item, index) {
-    var row = '';
+function renderBoard() {
+  var boardHTML = this.board.reduce(function(acc, item, index) {
+    var row = ''
     item.forEach(function (element, idx) {
       var pieceName, color, id, player = '';
       var classValue = element.background ? 'grey' : 'white';
@@ -55,77 +69,76 @@ function renderBoard () {
         id = 'data-piece=' + element.piece.id;
         player = 'data-player=' + color;
       }
-
-      row +=
-        '<div data-x=' + idx + ' class="' + classValue + '">' +
-            buildPiece(pieceName, color, id, player) +
+      
+      row += 
+        '<div data-x=' + idx + ' class="' + classValue + '">' +          
+            buildPiece(pieceName, color, id , player) + 
         '</div>';
-    });
-    acc += '<div data-y=' + index + ' class="row">' + row + '</div>';
-    return acc;
-  }, '');
+    })
+    return acc += '<div data-y=' + index + ' class="row">' + row + '</div>';
+  }, '');  
   BOARD_DOM.innerHTML = boardHTML;
 }
 
-function buildPiece (name, color, id, player) {
-  var value = '';
+function buildPiece(name, color, id , player) {
+  value = '';
   if (!name) return value;
-
-  if (name === PAWN) value = color === BLACK ? '&#9823;' : '&#9817;';
-  if (name === ROOK) value = color === BLACK ? '&#9820;' : '&#9814;';
-  if (name === KNIGHT) value = color === BLACK ? '&#9822;' : '&#9816;';
-  if (name === BISHOP) value = color === BLACK ? '&#9821;' : '&#9815;';
-  if (name === QUEEN) value = color === BLACK ? '&#9819;' : '&#9813;';
-  if (name === KING) value = color === BLACK ? '&#9818;' : '&#9812;';
-
+  
+  if (name === PAWN) value = color === BLACK ? B_PAWN : W_PAWN;
+  if (name === ROOK) value = color === BLACK ? B_ROOK : W_ROOK;
+  if (name === KNIGHT) value = color === BLACK ? B_KNIGHT : W_KNIGHT;
+  if (name === BISHOP) value = color === BLACK ? B_BISHOP : W_BISHOP;
+  if (name === QUEEN) value = color === BLACK ? B_QUEEN : W_QUEEN;
+  if (name === KING) value = color === BLACK ? B_KING : W_KING;
+  
   return '<div ' + id + ' ' + player + ' class="game-piece">' + value + '</div>';
 }
 
-function GamePiece (x, y, name, color, count) {
+function GamePiece(x, y, name, color, count) {
   this.name = name;
   this.color = color;
   this.x = x;
   this.y = y;
   this.id = name + count + color;
-  this.move(); // this will place piece in the board
+  this.move(); // this will place the piece in the board
   game.pieces[this.id] = this; // keep all pieces together for easy access
 }
 
-GamePiece.prototype.move = function () {
+GamePiece.prototype.move = function() {
   game.board[this.y][this.x].piece = this;
   game.render();
-};
+}
 
-GamePiece.prototype.update = function (x, y) {
+GamePiece.prototype.update = function(x, y) {
   if (isMoveAllowed(this, x, y)) {
     this.x = x;
     this.y = y;
     this.move();
     game.render();
-    game.updateTurn();
+    game.updateTurn(); 
   } else {
     this.goBack();
   }
-};
+}
 
-GamePiece.prototype.goBack = function () {
+GamePiece.prototype.goBack = function() {
   this.move();
-};
+}
 
-function isMoveAllowed (obj, x, y) {
+function isMoveAllowed(obj, x, y) {
   var isAllowed = false;
-
+  
   if (obj.name === PAWN) isAllowed = checkPawnRules(obj, x, y);
-
-  // *********************************************
+  
+  // ********************************************
   // **** TODO: add more rules for other pieces
   // *********************************************
-  if (obj.name === ROOK) return false;
+  if (obj.name === ROOK) isAllowed = checkRookRules(obj, x, y);
   if (obj.name === KNIGHT) return false;
   if (obj.name === BISHOP) return false;
   if (obj.name === QUEEN) return false;
   if (obj.name === KING) return false;
-
+  
   return isAllowed;
 }
 
@@ -147,7 +160,7 @@ function checkPawnRules (obj, x, y) {
       result = false;
     }
     // first move can jump 2 spaces
-    if (initialY === obj.y && y === obj.y - 2) {
+    if (initialY === obj.y && y === obj.y - 2 && obj.x === x) {
       result = true;
     }
   }
@@ -158,7 +171,7 @@ function checkPawnRules (obj, x, y) {
       result = false;
     }
     // first move can jump 2 spaces
-    if (initialY === obj.y && y === obj.y + 2) {
+    if (initialY === obj.y && y === obj.y + 2 && obj.x === x) {
       result = true;
     }
   }
@@ -173,29 +186,60 @@ function checkPawnRules (obj, x, y) {
   return result;
 }
 
-function drag (event) {
+function checkRookRules (obj, x, y) {
+  var dest = { x: x, y: y };
+  var collisionValue = checkCollision(x, y);
+  var ownColor = obj.color;
+  
+  // avoid moving in the same spot
+  if (x === obj.x && y === obj.y) return false;
+
+  // rook cant\'t move diagonal
+  if (x !== obj.x && y !== obj.y) return false;
+    
+  // checks which way is moving 
+  var letter = obj.x === x ? 'y' : 'x';
+   
+  // can not jump pieces
+  var min = Math.min(obj[letter], dest[letter]) + 1;
+  var max = Math.max(obj[letter], dest[letter]) - 1;
+  
+  for (var i = min; i <= max; i++) {
+    if (letter === 'y') {
+      if (checkCollision(x, i)) return false;
+    } else {
+      if (checkCollision(i, y)) return false;
+    }
+  }
+  
+  if (collisionValue && collisionValue.color !== ownColor || !collisionValue) return true;
+  
+  return false;
+}
+
+function drag(event) {
   if (event.target.classList.contains('game-piece')) {
     var element = event.target;
     var width = element.offsetWidth / 2;
     var height = element.offsetHeight / 2;
     var player = element.dataset.player;
-
+    
     var turn = game.turn ? BLACK : WHITE;
     // checks if player is draging his own pieces
     if (player === turn) isMoving = true;
-
-    element.addEventListener('mousemove', function (e) {
+    
+    element.addEventListener('mousemove', function(e) {
       if (isMoving) {
         var x = e.clientX - width;
         var y = e.clientY - height;
-
+        
         var board = BOARD_DOM.getBoundingClientRect();
         var coordX = x - board.x;
         var coordY = y - board.y;
-
-        // limits of the board
-        if (coordX < 0 || coordX > 375 || coordY < 0 || coordY > 375) return;
-
+        
+        // limits of the board        
+        if (coordX < 0 || coordX > 375 || coordY < 0 || coordY > 375 ) return
+                
         var position = 'left:' + x + 'px;top:' + y + 'px; z-index: 1;';
         element.setAttribute('style', position);
         element.classList.add('active');
@@ -204,9 +248,9 @@ function drag (event) {
   }
 }
 
-function drop (event) {
+function drop(event) {
   if (isMoving) {
-    var element = event.target;
+    var element = event.target
     var x = event.x;
     var y = event.y;
 
@@ -219,48 +263,48 @@ function drop (event) {
   isMoving = false;
 }
 
-function getCoordinates (x, y) {
+function getCoordinates(x, y) {
   var board = BOARD_DOM.getBoundingClientRect();
-
-  var coordX = x - board.x - BOARD_BORDER;
+  
+  var coordX = x - board.x - BOARD_BORDER; 
   var coordY = y - board.y - BOARD_BORDER;
-
+  
   const boardSize = ROW * SQUARE_SIZE;
   var resultX = Math.floor(coordX / boardSize * ROW);
   var resultY = Math.floor(coordY / boardSize * ROW);
-
+  
   return { x: resultX, y: resultY };
 }
 
-function updateBoard (element, coord) {
+function updateBoard(element, coord) {
   var x = coord.x;
   var y = coord.y;
-  var id = element.dataset.piece;
+  var id = element.dataset.piece;  
   var piece = game.pieces[id];
-
+    
   // erase piece from initial coordinates
   game.board[piece.y][piece.x].piece = null;
   // update piece
   piece.update(x, y);
+} 
+
+function checkCollision(x, y) {
+  return (game.board[y][x].piece) 
 }
 
-function checkCollision (x, y) {
-  return (game.board[y][x].piece);
-}
-
-function updateTurn () {
+function updateTurn() {
   this.turn = !this.turn;
-
+  
   var classValue = this.turn ? 'player-black' : 'player-white';
   var player = this.turn ? 'Black' : 'White';
   var feedBack = '<div class="' + classValue + '">Next: ' + player + '</div>';
-
+  
   turnDom.innerHTML = feedBack;
 }
 
-// -----------------------------------------------------------------------------
+// ---------------------------------------------------
 // Game Module
-// -----------------------------------------------------------------------------
+// ---------------------------------------------------
 
 var game = {
   board: createBoard(ROW, COL),
@@ -268,7 +312,7 @@ var game = {
   pieces: {},
   turn: true,
   updateTurn: updateTurn,
-  init: function () {
+  init: function() {
     BOARD_DOM.addEventListener('mousedown', drag);
     BOARD_DOM.addEventListener('mouseup', drop);
 
@@ -278,10 +322,10 @@ var game = {
     }
 
     // create white pawns
-    for (var j = 0; j < 8; j++) {
-      new GamePiece(j, 6, PAWN, WHITE, j);
+    for (var i = 0; i < 8; i++) {
+      new GamePiece(i, 6, PAWN, WHITE, i);
     }
-
+    
     new GamePiece(0, 7, ROOK, WHITE, 1);
     new GamePiece(7, 7, ROOK, WHITE, 2);
     new GamePiece(1, 7, KNIGHT, WHITE, 1);
@@ -290,7 +334,7 @@ var game = {
     new GamePiece(5, 7, BISHOP, WHITE, 2);
     new GamePiece(3, 7, QUEEN, WHITE, 1);
     new GamePiece(4, 7, KING, WHITE, 1);
-
+    
     new GamePiece(0, 0, ROOK, BLACK, 1);
     new GamePiece(7, 0, ROOK, BLACK, 2);
     new GamePiece(1, 0, KNIGHT, BLACK, 1);
@@ -302,7 +346,7 @@ var game = {
 
     this.updateTurn();
     this.render();
-  }
-};
+  } 
+}
 
 game.init();
