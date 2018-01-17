ed4e40e7c2c0dd6f63e67b635d4bd74abaa2ece3
Pablo Jurado <pjurado85@gmail.com>
2018-01-17 13:06:56 -0600

    create board with ng-repeat

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-console.log('hi');
+/* global angular */
+
+var chess = angular.module('chess', []);
+
+chess.controller('chessController', function($scope) {
+  $scope.board = createBoard(8, 8);
+});
+
+function createBoard(row, col) {
+  var arr = [];
+  for (var i = 0; i < row; i++) {
+    var rowArr = [];
+    for (var j = 0; j < col; j++) {
+      var value = true;
+      if (i % 2 === 0) value = !value;
+      if (j % 2 === 0) value = !value;
+      rowArr.push({ piece: null, background: value });
+    }
+    arr.push(rowArr);
+  }
+  return arr;
+}
